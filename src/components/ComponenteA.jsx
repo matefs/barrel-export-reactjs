f450d98672316c20feda3bdc54d0270868303283
matefs <matefs8569@gmail.com>
2023-07-30 15:26:05 -0300

    1 file modified

--- src/components/ComponenteA.jsx
+++ src/components/ComponenteA.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ComponenteA = () => {
-  return <div style={{backgroundColor:'rgba(255,255,255,.3)'}}>Este é o Componente A</div>;
+  return (
+    <div style={{ margin: '2%', backgroundColor: 'rgba(255,255,255,.3)' }}>
+      Este é o Componente A
+    </div>
+  );
 };
 
 export default ComponenteA;
